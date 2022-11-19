69d207899a1cc060d96af59ece8da9d2923a63e5
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-11-19 18:37:41 -0300

    bobobobora

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const transactionsUl = document.querySelector("#transactions");
-const incomeDisplay = document.querySelector('#money-plus')
-const expenseDisplay = document.querySelector('#money-minus')
-const balanceDisplay = document.querySelector('#balance')
-
+const incomeDisplay = document.querySelector("#money-plus");
+const expenseDisplay = document.querySelector("#money-minus");
+const balanceDisplay = document.querySelector("#balance");
+const form = document.querySelector("#form");
+const inputTransactionName = document.querySelector("#text");
+const inputTransactionAmount = document.querySelector("#amount");
 
 const dummyTransactions = [
 	// cada objeto tem um id, name e amount
@@ -35,32 +37,34 @@ const addTransactionIntoDOM = (transaction) => {
 	transactionsUl.append(li); // isso adiciona como ULTIMO filho PRONTO
 };
 
-
 //-----------------------------Atualizar o Saldo, receita e despesas
 const updateBalanceValues = () => {
 	//fazer um map pq ele é um tipo forEach, só que retorna um novo array só com os values, ai eu uso um reduce pra transformar num valor único
-	const transactionsAmount = dummyTransactions.map(transaction => transaction.amount)
+	const transactionsAmount = dummyTransactions.map(
+		(transaction) => transaction.amount
+	);
 	const total = transactionsAmount
-		.reduce((accumulator, transaction) => accumulator + transaction, 0).toFixed(2)
-												
+		.reduce((accumulator, transaction) => accumulator + transaction, 0)
+		.toFixed(2);
+
 	const income = transactionsAmount
-		.filter(value => value > 0)
-		.reduce((accumulator, transaction) => accumulator +transaction, 0).toFixed(2)
+		.filter((value) => value > 0)
+		.reduce((accumulator, transaction) => accumulator + transaction, 0)
+		.toFixed(2);
 
-	const expense = transactionsAmount
-	.filter(value => value <0)
-	.reduce((accumulator, transaction) => accumulator +transaction, 0).toFixed(2)
-// até aqui eu consegui já: balançoT, receitaT e despesaT, agora preciso adicionar no dom
-//pra adicionar eu preciso pegar as referências de cada, no caso saldo = h1, receitas = p e despesas = p
-//as referências foram feitas e colocadas lá em cima, no começo. income/expense/balanceDisplay
+	const expense = Math.abs(
+		transactionsAmount //adicionado o ABS aqui, e DEPOIS colocado o toFixed 2, assim sempre fica 2 casas decimais
+			.filter((value) => value < 0)
+			.reduce((accumulator, transaction) => accumulator + transaction, 0)
+	).toFixed(2);
+	// até aqui eu consegui já: balançoT, receitaT e despesaT, agora preciso adicionar no dom
+	//pra adicionar eu preciso pegar as referências de cada, no caso saldo = h1, receitas = p e despesas = p
+	//as referências foram feitas e colocadas lá em cima, no começo. income/expense/balanceDisplay
 
-	balanceDisplay.textContent = `R$ ${total}`
-	incomeDisplay.textContent = `R$ ${income}`
-	expenseDisplay.textContent = `- R$ ${Math.abs(expense)}`
-}
-
-
-
+	balanceDisplay.textContent = `R$ ${total}`;
+	incomeDisplay.textContent = `R$ ${income}`;
+	expenseDisplay.textContent = `- R$ ${expense}`;
+};
 
 //---------------------------------- init precisa ficar aqui embaixo pq o JS precisa ler as outras funções antes.
 // qnd a pagina for carregada é para chamar as transações armazenadas
@@ -68,8 +72,41 @@ const init = () => {
 	//preciso fazer um loop para iterarar cada transação armazenada no dummyTransactions (q é o objto que guarda id, name, amount)
 	dummyTransactions.forEach(addTransactionIntoDOM);
 	// eu estou chamando o objeto e para CADA item do objeto eu realizo a função que cria li e adc
-	
-//-------------------------------Atualizar o saldo e receita e despesas
-updateBalanceValues()
-}
+
+	//-------------------------------Atualizar o saldo e receita e despesas
+	updateBalanceValues();
+};
 init();
+
+//pode ser deopis do init, pq só tem ocmo tu enviar um form se já tiver carregado xD
+//------------------------- adicionando uma transação -----------------
+//precisa de uma lista de eventos para quando o form for enviado, começamos então buscando a ref do form
+
+const generateID = () => Math.round(Math.random() * 1000); // assim eu gero um numero aleatório de 0 a 1000
+
+form.addEventListener("submit", (event) => {
+	event.preventDefault(); //prevenir de realmente enviar (por enquanto)
+
+	const transactionName = inputTransactionName.value.trim();
+	const transactionAmount = inputTransactionAmount.value.trim();
+
+	//agora preciso ter certeza de que os valores do nome e do vlaor foram realmente preenchidos
+	// logo, preciso da ref dos 2 inputs
+	if (transactionName === "" || transactionAmount === "") {
+		alert("Por favor, preencha tanto o nome quanto o valor da transação");
+		return; // esse return faz com que, CASO seja lido esse bloco do if, ele da um RETURN e acaba a leitura da função
+	}
+
+	// agora preciso fazer um objeto igual ao dos dummyTransactions
+	// ou seja, com propriedades: id, value, amount
+	const transaction = {
+		id: generateID(),
+		name: transactionName,
+		amount: Math.floor(transactionAmount) // precisa colocar o Math.floor pq senão ele entende que é uma string, não um num
+	};
+	// agora cheguei numa questão, e o valor da propriedade ID? ql vai ser?
+	// vamos fazer através da geração de id's aleatórios (generateID)
+	// agora eu preciso adicionar ele no array dummyTransctions, clássico push
+	dummyTransactions.push(transaction)
+	console.log(dummyTransactions)
+});
